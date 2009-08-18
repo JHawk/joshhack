4c68cb9db14f4e066871e341def68d16ec2e0fce
josh Hawkins <jhawkins@josh-desktop.(none)>
2009-08-18 10:44:00 -0400

    made player able to walk on water and over stairs

--- src/joshhack/sprite.clj
+++ src/joshhack/sprite.clj
@@ -5,8 +5,6 @@
 ;;;; 
 ;;;; Sprites Generation
 
-(def sprites-map {})
-
 (defn- add-sprite
   "Adds an object, replaces an existing floor tile with type tile"
   [world tile sprites]
@@ -16,9 +14,9 @@
 (defn gen-sprites
   "Adds starting sprites"
   [world]
-  (add-sprite world :player 
+  (add-sprite world :stairs-down
 	      (add-sprite world :stairs-up 
-			  (add-sprite world :stairs-down sprites-map))))
+			  (add-sprite world :player {}))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; 
@@ -28,9 +26,9 @@
   "Changes the players location if the new location is legal"
   [player w x y]
   (if (and (< 0 x) 
-	   (< 0 y) 
-	   (> (count w) x) 
+	   (< 0 y)
+	   (> (count w) x)
 	   (> (count (first w)) y)
-	   (world/is-symbol? w y x :floor))
+	   (or (world/is-symbol? w y x :floor) (world/is-symbol? w y x :water)))
     [x y]
     player))
